012f1b6700118de2c20f8bab7b3a8296f7668695
bhupendrasinghacme <saumyayadav@acmeintech.in>
2024-12-09 19:09:40 +0530

    soumya home page

--- assets/global.js
+++ assets/global.js
@@ -1312,16 +1312,10 @@ document.addEventListener('DOMContentLoaded', function () {
       loop: true,
       slidesPerView: 3,
       spaceBetween: 10,
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
+      freeMode: true,
+      speed: 5000,
       autoplay: {
-        delay: 5000,
+        delay: 0,
         disableOnInteraction: false,
       },
     });
