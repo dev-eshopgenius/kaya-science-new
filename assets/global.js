227a9afa963704817fd835e4f590bb19b25675f3
bhupendrasinghacme <saumyayadav@acmeintech.in>
2024-12-23 13:30:15 +0530

    soumya

--- assets/global.js
+++ assets/global.js
@@ -1271,7 +1271,6 @@ if (!customElements.get('bulk-add')) {
 
 // announcemnet and text slider js start
 const swiperContainers = document.querySelectorAll('.swiper-slider-wrap');
-
 swiperContainers.forEach((container) => {
   new Swiper(container, {
     autoplay: {
@@ -1292,7 +1291,34 @@ swiperContainers.forEach((container) => {
 // collection slider js start
 var colswiperContainer = document.querySelector('.home-collection-slider');
 if (colswiperContainer) {
-var col_swiper = new Swiper('.home-collection-slider', {
+    var col_swiper = new Swiper('.home-collection-slider', {
+        loop: true,
+        spaceBetween: 24,
+        autoplay: {
+            delay: 1000,
+            pauseOnMouseEnter: true,
+            disableOnInteraction: false,
+        },
+        loopAddBlankSlides: true,
+        cssMode: false,
+        slidesPerView: 4,
+        speed: 2000,
+        grabCursor: true,
+        breakpoints: {
+            // When the viewport width is <= 768px (for mobile)
+            768: {
+                slidesPerView: 1, // Show only one slide on mobile
+            },
+        },
+    });
+}
+
+}
+// collection slider js end
+
+var prod_Container = document.querySelector('.main-slider');
+if (prod_Container) {
+var pro_swiper = new Swiper('.main-slider', {
 loop: true,
 spaceBetween: 24,
 autoplay: {
@@ -1307,28 +1333,24 @@ speed: 2000,
 grabCursor: true,
 });
 }
-// collection slider js end
 
+// document.addEventListener("DOMContentLoaded", () => {
+//   const mainSlider = new Swiper(".main-slider", {
+//     spaceBetween: 10,
+//     slidesPerView: 1,
+//     loop: true,
+//   });
 
+//   const thumbnailSlider = new Swiper(".thumbnail-slider", {
+//     spaceBetween: 10,
+//     slidesPerView: 4, 
+//     freeMode: true,
+//     watchSlidesProgress: true,
+//   });
 
-document.addEventListener("DOMContentLoaded", () => {
-  const mainSlider = new Swiper(".main-slider", {
-    spaceBetween: 10,
-    slidesPerView: 1,
-    loop: true,
-  });
-
-  const thumbnailSlider = new Swiper(".thumbnail-slider", {
-    spaceBetween: 10,
-    slidesPerView: 4, 
-    freeMode: true,
-    watchSlidesProgress: true,
-  });
-
-  // Connect the two sliders
-  mainSlider.controller.control = thumbnailSlider;
-  thumbnailSlider.controller.control = mainSlider;
-});
+//   mainSlider.controller.control = thumbnailSlider;
+//   thumbnailSlider.controller.control = mainSlider;
+// });
 
 
 
